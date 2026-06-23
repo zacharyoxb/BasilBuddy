b64659bec87dda501adb321db8833c988c18002c
zacharyoxb <oxboroughzachary@protonmail.com>
2026-06-23 15:32:41 +0100

    Added basic interrupts

--- BasilBuddy.c
+++ BasilBuddy.c
@@ -1,29 +1,54 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/adc.h"
+#include "hardware/timer.h"
 
 const int ADC0 = 0;
 const int TEMP_SENSOR = 4;
-const float CONV_FACTOR = 3.3f / (1 << 12);
+
+typedef struct
+{
+    volatile uint16_t moisture_raw;
+    volatile uint16_t temp_raw;
+    volatile uint16_t timestamp;
+} sensor_data_t;
+
+static struct repeating_timer timer;
+static sensor_data_t LAST_READING = {0};
+
+void adc_setup()
+{
+    adc_init();
+    adc_gpio_init(ADC_BASE_PIN);
+    adc_set_temp_sensor_enabled(true);
+}
+
+void send_bt_packet() {}
+
+bool read_sensors_callback(struct repeating_timer *t)
+{
+    adc_select_input(ADC0);
+    LAST_READING.moisture_raw = adc_read();
+    adc_select_input(TEMP_SENSOR);
+    LAST_READING.temp_raw = adc_read();
+    LAST_READING.timestamp = get_absolute_time();
+
+    send_bt_packet();
+
+    return true;
+}
+
+void init_timer_interrupt(struct repeating_timer timer)
+{
+    add_repeating_timer_ms(1000, read_sensors_callback, NULL, &timer);
+}
 
 int main()
 {
     stdio_init_all();
-    adc_init();
-    adc_gpio_init(ADC_BASE_PIN);
-
-    adc_set_temp_sensor_enabled(true);
-    int pin_mask = (1 << ADC0) | (1 << TEMP_SENSOR);
-    adc_set_round_robin(pin_mask);
-    adc_select_input(ADC0);
+    adc_setup();
 
     while (true)
-    {
-        // Read from moisture sensor first
-        uint16_t moist_output = adc_read();
-        printf("Moisture output: %f\n", moist_output * CONV_FACTOR);
-
-        // Now read from temperature sensor
-        uint16_t temp_output = adc_read();
-        printf("Temp output: %f\n", temp_output * CONV_FACTOR);
-    }
+        ;
 }
